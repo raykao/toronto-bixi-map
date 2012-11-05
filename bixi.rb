3cf5ad145a4d7f9a2153b3b3024313555211eed2
Raymond Kao <ray@peopleandcode.com>
2012-11-04 20:37:13 -0500

    daemonized with thin

--- bixi.rb
+++ bixi.rb
@@ -5,25 +5,31 @@ require 'active_support/core_ext/hash'
 require 'json'
 require 'coffee-script'
 
-def get_bixi_data
-	con = Faraday.new
-	request = con.get 'https://toronto.bixi.com/data/bikeStations.xml'
-	request.body
-end
+class Bixi < Sinatra::Base
 
-get '/' do
-	if Time.now > (File.mtime('public/latest.json') + 1.minutes)
-		data = get_bixi_data
-		data = Hash.from_xml(data).to_json
-		file = File.new('public/latest.json', 'w')
-		file.write(data)
-		file.close
-		"Generated new latest.json"
+	def get_bixi_data
+		con = Faraday.new
+		request = con.get 'https://toronto.bixi.com/data/bikeStations.xml'
+		request.body
 	end
 
-	erb :index
+	get '/' do
+		if Time.now > (File.mtime('public/latest.json') + 1.minutes)
+			data = get_bixi_data
+			data = Hash.from_xml(data).to_json
+			file = File.new('public/latest.json', 'w')
+			file.write(data)
+			file.close
+			"Generated new latest.json"
+		end
+
+		erb :index
+	end
+
+	get '/application.js' do 
+		coffee :application
+	end
+
 end
 
-get '/application.js' do 
-	coffee :application
-end
+Bixi.run!
---
+++ config.ru
@@ -0,0 +1,3 @@
+require './bixi'
+
+run Sinatra::Application
